eb8f87e7faf4fc78f6c1d1692dd255fc56890549
Osman <osmanhajr32@gmail.com>
2020-11-22 19:35:22 +0000

    mapping the classes array

--- src/pages/ChooseClass/ChooseClass.jsx
+++ src/pages/ChooseClass/ChooseClass.jsx
@@ -3,13 +3,12 @@ import './chooseclass.scss'
 import dummyData from './dummyData.json'
 import { useHistory } from "react-router-dom";
 
-
 const ChooseClass = () => {
- 
   let history = useHistory();
   function handleClick() {
     history.push("/students-tracker");
   }
+  
   return(
     <div className="container">
       <div className="welcome-msg">
@@ -17,15 +16,8 @@ const ChooseClass = () => {
         <p>Please choose which class would you like to access:</p>
       </div>
       <div className="btnDiv">
-        <button type="button" onClick={handleClick}>
-          {dummyData[0].classes[0]}
-        </button>
-        <button type="button" onClick={handleClick}>
-          {dummyData[0].classes[1]}
-        </button>
-        <button type="button" onClick={handleClick}>
-          {dummyData[0].classes[2]}
-        </button>
+        {dummyData[0].classes.map(x =>
+        (<button type="button" onClick={handleClick}>{x}</button>))}
       </div>
     </div>
   )
