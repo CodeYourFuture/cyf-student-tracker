067db7b35a7ab3e66723c8522fe3b7bf181e51c5
Osman <osmanhajr32@gmail.com>
2020-11-22 13:17:34 +0000

    choose-class home page contents

---
+++ src/pages/ChooseClass/ChooseClass.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import './chooseclass.scss'
+import dummyData from './dummyData.json'
+import { useHistory } from "react-router-dom";
+//import classes from '*.module.css';
+
+const ChooseClass = () => {
+ 
+  let history = useHistory();
+  function handleClick() {
+    history.push("/students-tracker");
+  }
+  return(
+    <div>
+      <div>
+        <h3>Welcome back <span>{dummyData[0].userName} </span>!</h3>
+        <p>Please choose which class would you like to access:</p>
+      </div>
+      <div>
+        <button type="button" onClick={handleClick}>
+          {dummyData[0].classes[0]}
+        </button>
+        <button type="button" onClick={handleClick}>
+          {dummyData[1].classes[1]}
+        </button>
+        <button type="button" onClick={handleClick}>
+          {dummyData[2].classes[2]}
+        </button>
+      </div>
+    </div>
+  )
+}
+export default ChooseClass
